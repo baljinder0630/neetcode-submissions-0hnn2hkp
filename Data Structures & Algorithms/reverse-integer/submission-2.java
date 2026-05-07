712287568a2782d00a29d2defbefa8de4882b365
Baljinder singh <123756514+baljinder0630@users.noreply.github.com>
2026-05-07 09:08:19 +0530

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while(x != 0){
+            int last = x % 10;
+            x /= 10;
+            if(ans > Integer.MAX_VALUE / 10 || ans < Integer.MIN_VALUE / 10)return 0;
+            ans *= 10;
+            ans += last;
+        }
+        return ans;
+    }
+}
